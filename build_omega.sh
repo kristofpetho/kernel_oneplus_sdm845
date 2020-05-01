a26c9260148035004039befe692215969e772689
Kristof Petho <kristof.petho@gmail.com>
2020-05-01 19:55:13 +0200

    build_omega.sh: switch to custom Clang build

--- build_omega.sh
+++ build_omega.sh
@@ -17,8 +17,8 @@ echo
 echo "Set DEFCONFIG"
 echo 
 make O=out omega_defconfig
-export PATH="/home/kristof/android/clang/bin:/home/kristof/android/binutils/bin:${PATH}"
-export LD_LIBRARY_PATH="/home/kristof/android/clang/lib:/home/kristof/android/clang/lib64:$LD_LIBRARY_PATH"
+export PATH="/home/kristof/android/clang/bin:${PATH}"
+export LD_LIBRARY_PATH="/home/kristof/android/clang/lib:$LD_LIBRARY_PATH"
 
 echo
 echo "Build The Kernel"
@@ -26,5 +26,4 @@ echo
 
 make -j$(nproc --all) O=out \
                       CC=clang \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
                       CROSS_COMPILE=aarch64-linux-gnu-
